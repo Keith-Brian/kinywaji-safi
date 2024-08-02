433a0600b37da14460a0de05fb44477c2f36ee6b
keith-brian <chiefdeveloper254@gmail.com>
2024-08-02 21:58:13 +0300

    configured splash-screen

--- app/src/main/java/com/devkaybee/kinywajisafi/MainActivity.java
+++ app/src/main/java/com/devkaybee/kinywajisafi/MainActivity.java
@@ -1,24 +1,84 @@
 package com.devkaybee.kinywajisafi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    FirebaseUser user;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        reference = FirebaseDatabase.getInstance().getReference();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(user == null){
+                    Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    String userId = user.getUid();
+                    DatabaseReference userRef = reference.child("users").child(userId);
+
+                    userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.exists()){
+                                String role = snapshot.getValue(String.class);
+
+                                Intent intent;
+                                if(role.equals("Admin")){
+                                    intent = new Intent(MainActivity.this, AdminViewOrder.class);
+                                } else {
+                                    intent = new Intent(MainActivity.this, CustomerMakeOrder.class);
+                                }
+                                startActivity(intent);
+                                finish();
+
+                            }else {
+                                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+
+                }
+            }
+        },2000);
+
     }
 }
